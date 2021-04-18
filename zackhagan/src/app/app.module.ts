645e21cbfe675eb1ba5be0b8687da66a64dbc948
zhagan <zhagan@gmail.com>
2021-04-17 19:40:18 -0600

    angular init

--- zackhagan/src/app/app.module.ts
+++ zackhagan/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MusicianComponent } from './musician/musician.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MusicianComponent
   ],
   imports: [
     BrowserModule,
