268cb4eeb559148528146d171970424395c705b2
zhagan <zhagan@gmail.com>
2021-04-17 19:47:24 -0600

    angular init

--- zackhagan/src/app/musician/musician.component.spec.ts
+++ zackhagan/src/app/musician/musician.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MusicianComponent } from './musician.component';
 
@@ -6,7 +6,7 @@ describe('MusicianComponent', () => {
   let component: MusicianComponent;
   let fixture: ComponentFixture<MusicianComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MusicianComponent ]
     })
