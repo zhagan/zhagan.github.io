5a3593c4105ba81144926937e4db276b5c7414af
zhagan <zhagan@gmail.com>
2021-04-20 19:57:51 -0600

    music starter

--- zackhagan/src/app/app-routing.module.ts
+++ zackhagan/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MusicianComponent } from './musician/musician.component';
+import { DeveloperComponent } from './developer/developer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'music', component: MusicianComponent },
+  { path: 'code', component: DeveloperComponent },
+  { path: '', component: MusicianComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
--- zackhagan/src/app/app.module.ts
+++ zackhagan/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MusicianComponent } from './musician/musician.component';
+import { DeveloperComponent } from './developer/developer.component';
+import { FooterComponent } from './footer/footer.component';
+import { AlbumComponent } from './album/album.component';
+import { SafePipe } from './safe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MusicianComponent
+    MusicianComponent,
+    DeveloperComponent,
+    FooterComponent,
+    AlbumComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
